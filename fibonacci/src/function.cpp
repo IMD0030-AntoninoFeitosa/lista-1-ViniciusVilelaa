8d025ecae28100c8f08bd6ebfa1d7f310b2198f6
Vinicius Bezerra Vilela <vinicius.vilela.105@ufrn.edu.br>
2022-04-09 18:37:21 -0300

    Update function.cpp

--- fibonacci/src/function.cpp
+++ fibonacci/src/function.cpp
@@ -1,25 +1,19 @@
 #include "function.h"
 
+
 std::vector<unsigned int> fib_below_n( unsigned int n )
 {
-    int termo_1 = 0, termo_2 = 1, p_termo = 0;
-    vector<unsigned int> vect;
+    unsigned int sequencia = 0;
+    unsigned int a = 0 , b = 1;
 
-    for (int i = 1; i <= n; ++i) {
-        if(i == 1) {
-            vect.push_back(termo_1);
-            continue;
-        }
-        if(i == 2) {
-            vect.push_back(termo_2);
-            continue;
-        }
-        p_termo = termo_1 + termo_2;
-        termo_1 = termo_2;
-        termo_2 = p_termo;
-        
-        vect.push_back(p_termo);
+
+    for( int i = 0; i < n; i++)
+    {
+        sequencia = (a + b);
+        a = b;
+        b = sequencia;
     }
-    
-    return vect;
+            
+ 
+    return std::vector<unsigned int>{b};
 }
