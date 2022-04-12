f8839693a6f2906f2a4d5873fffad356c8ceab4c
Vinicius Bezerra Vilela <vinicius.vilela.105@ufrn.edu.br>
2022-04-12 11:41:04 -0300

    MINMAX CONCLUIDA

--- minmax/src/function.cpp
+++ minmax/src/function.cpp
@@ -10,10 +10,32 @@
  * @return A pair of indexes to the first smallest and last largest values.
  */
 
-std::pair<int,int> min_max( int V[], size_t n )
+std::pair<int,int> min_max( int V[], std::size_t n )
 {
-    // TODO: Adicione aqui sua solução.
+    int x = 0, auxiliar_MAX = 0, auxiliar_MIN = 0;
+    std::pair<int,int> par;
+    par.first=V[0], par.second=V[0];
 
-    // TODO: Isso é apenas um STUB. Substitua com seu retorno correto.
-    return { -1, -1 };
+    /* CONTADOR */
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(V[i]<V[j]){
+                auxiliar_MIN = V[i];
+                if(auxiliar_MIN < par.first){
+                    par.first = auxiliar_MIN;
+                }
+            }
+            else if(V[i]>V[j]){
+                auxiliar_MAX = V[i];
+                if(auxiliar_MAX > par.second){
+                    par.second = auxiliar_MAX;
+                }
+
+            }
+
+        }
+    }
+
+    return par;
 }
